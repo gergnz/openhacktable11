c9180cd8185989d00e9a43bfdd1dc07bb938fb8d
Yashwanth <yyellapragada@gmail.com>
2018-06-19 12:58:01 +1000

    Added basic list functions

--- apidemo.py
+++ apidemo.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+from kubernetes import client, config
+import json
 app = Flask(__name__)
 api = Api(app)
 
 
 class List(Resource):
     def get(self):
-        return "Showing the resources"
+        output = []
+        config.load_kube_config()
+        v1 = client.CoreV1Api()
+        print("Listing pods with their IPs:")
+        ret = v1.list_pod_for_all_namespaces(watch=False)
+
+        for i in ret.items:
+            eachOutput = {}
+            eachOutput['name']=i.metadata.name
+            eachOutput['ip']=i.status.pod_ip
+            output.append(eachOutput)
+        return output
 
 class Add(Resource):
     def get(self):
